dc86f6bca46a5e8d28e0f01886331a503c0104ea
Happid <happidilmi@gmail.com>
2025-12-19 11:26:14 +0700

    initial project spring boot

---
+++ allo-be/src/main/java/com/example/allo_be/AlloBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.allo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloBeApplication.class, args);
+	}
+
+}
---
+++ allo-be/src/test/java/com/example/allo_be/AlloBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlloBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
